0a8c9ee309a8fdbe10d4b72a1e177864d2dcf376
yashsrepo <yashk6767@gmail.com>
2024-03-21 22:24:34 +0530

    starting a mern social media App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 
-function App() {
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+      <Route path="/" element={<Users />}/>
+      <Route path="/places/new" element={<NewPlace />}/>
+          
+
+      </Routes>
+      
+    </Router>
   );
-}
+};
 
 export default App;
+//<Switch>
+        {/* <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          
+        </Route>
+        <Redirect to="/" />
+      </Switch> */}
