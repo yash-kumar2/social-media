4c6ca96266083552e613d3748e0c702ff390945e
yashsrepo <yashk6767@gmail.com>
2024-03-21 22:39:10 +0530

    creating the MainHeader

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
-
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
   return (
     <Router>
+      <MainNavigation/>
+      <main>
       <Routes>
       <Route path="/" element={<Users />}/>
       <Route path="/places/new" element={<NewPlace />}/>
           
 
       </Routes>
-      
+      </main>
     </Router>
   );
 };
